a974c67c4bc390444e65702b3c94e480196c7040
Šimon Lukašík <slukasik@redhat.com>
2014-07-28 10:50:15 +0200

    tests: Skeleton for testing rails engine.

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'b14c9075deb291007ea7ef1f8cd20b5b83abfcbde0e61587a7ed0393702bb651589060d8a57631ffb55033507955b7008d019969e03b28f113eddd12349d8ff4'
