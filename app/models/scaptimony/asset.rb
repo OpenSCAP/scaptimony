d45189ec5368571bca5d1e6045bb010f6e9ab091
Šimon Lukašík <slukasik@redhat.com>
2014-10-14 12:54:54 +0200

    Introduce Asset class to SCAPtimony model
    An object of category (#Asset) is every auditable compute resource.

---
+++ app/models/scaptimony/asset.rb
@@ -0,0 +1,4 @@
+module Scaptimony
+  class Asset < ActiveRecord::Base
+  end
+end
---
+++ test/models/scaptimony/asset_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+module Scaptimony
+  class AssetTest < ActiveSupport::TestCase
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
