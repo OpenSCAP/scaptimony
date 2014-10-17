a1fdb04de4e55cbc453758ccf0b5c15dfb5016d7
Šimon Lukašík <slukasik@redhat.com>
2014-10-17 13:56:24 +0200

    Remove overabundant parenthesis
    Addressing:
    /usr/sbin/foreman-rake db:migrate
    rake aborted!
    /opt/rh/ruby193/root/usr/share/gems/gems/scaptimony-0.0.1/db/migrate/20141015115511_add_arf_report_unique_constraint.rb:3:
    syntax error, unexpected ')', expecting keyword_end

--- db/migrate/20141015115511_add_arf_report_unique_constraint.rb
+++ db/migrate/20141015115511_add_arf_report_unique_constraint.rb
@@ -1,5 +1,5 @@
 class AddArfReportUniqueConstraint < ActiveRecord::Migration
   def change
-    add_index :scaptimony_arf_reports, [:assert_id, :policy_id, :date, :digest], :unique => true)
+    add_index :scaptimony_arf_reports, [:assert_id, :policy_id, :date, :digest], :unique => true
   end
 end
