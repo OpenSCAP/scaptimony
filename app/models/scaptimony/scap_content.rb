db11636035a27132d2db564fc6348b03baec15aa
Šimon Lukašík <slukasik@redhat.com>
2014-11-11 11:11:07 +0100

    Introduce Scaptimony::ScapContent.store! method.

--- app/models/scaptimony/scap_content.rb
+++ app/models/scaptimony/scap_content.rb
@@ -1,4 +1,43 @@
+require 'digest'
+require 'fileutils'
+require 'openscap/source'
+require 'scaptimony/engine'
+
 module Scaptimony
   class ScapContent < ActiveRecord::Base
+    attr_writer :scap_file
+
+    def store
+      begin
+        FileUtils.mkdir_p dir
+        source.save path
+      rescue StandardError => e
+        errors[:base] << e.message
+        return false
+      end
+      save
+    end
+
+    def source
+      @source ||= source_init
+    end
+
+    def digest
+      @digest ||= Digest::SHA256.hexdigest @scap_file
+    end
+
+    private
+    def source_init
+      OpenSCAP.oscap_init
+      OpenSCAP::Source.new(:content => @scap_file, :path => path)
+    end
+
+    def path
+      "#{dir}/#{digest}"
+    end
+
+    def dir
+      "#{Scaptimony::Engine.dir}/content"
+    end
   end
 end
