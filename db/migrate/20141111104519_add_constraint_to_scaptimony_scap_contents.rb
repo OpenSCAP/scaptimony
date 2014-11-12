8669c063b2d614ea6e2f9e0765de5dffc2052c97
Šimon Lukašík <slukasik@redhat.com>
2014-11-13 00:12:17 +0100

    Fix incorrect usage of change_column
    Addressing:
    -- change_column(:scaptimony_scap_contents, :title, {:null=>false})
    rake aborted!
    An error has occurred, this and all later migrations canceled:
    
    undefined method `to_sym' for {:null=>false}:Hash

--- db/migrate/20141111104519_add_constraint_to_scaptimony_scap_contents.rb
+++ db/migrate/20141111104519_add_constraint_to_scaptimony_scap_contents.rb
@@ -1,6 +1,6 @@
 class AddConstraintToScaptimonyScapContents < ActiveRecord::Migration
   def change
-    change_column :scaptimony_scap_contents, :title, :null => false
-    change_column :scaptimony_scap_contents, :digest, :null => false
+    change_column :scaptimony_scap_contents, :title, :string, :null => false
+    change_column :scaptimony_scap_contents, :digest, :string, :null => false
   end
 end
