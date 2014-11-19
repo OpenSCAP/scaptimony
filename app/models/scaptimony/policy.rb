1993532c76a3ea4361a546fd665fe1714ec47442
Šimon Lukašík <slukasik@redhat.com>
2014-11-19 17:36:23 +0100

    Make sure that policy name is always present.

--- app/models/scaptimony/policy.rb
+++ app/models/scaptimony/policy.rb
@@ -6,6 +6,8 @@ module Scaptimony
     belongs_to :scap_content
     belongs_to :scap_content_profile
     has_many :arf_reports, dependent: :destroy
+
+    validates :title, :presence => true
   end
 
   class GuideGenerator
