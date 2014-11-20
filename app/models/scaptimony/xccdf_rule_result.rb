429bd07c6cacd34267692983b3ae67c35a741181
Šimon Lukašík <slukasik@redhat.com>
2014-11-20 14:21:07 +0100

    Search XccdfRuleResults based on the result
    This should be easier than defining 9 distinct scopes.

--- app/models/scaptimony/xccdf_rule_result.rb
+++ app/models/scaptimony/xccdf_rule_result.rb
@@ -3,5 +3,9 @@ module Scaptimony
     belongs_to :arf_report
     belongs_to :xccdf_result
     belongs_to :xccdf_rule
+
+    def self.f result_name
+      includes(:xccdf_result).where("scaptimony_xccdf_results.name = '#{result_name}'")
+    end
   end
 end
