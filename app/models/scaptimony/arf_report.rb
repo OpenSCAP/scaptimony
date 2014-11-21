a7d28107bf042d22db8a3f8292a183068fe468e1
Šimon Lukašík <slukasik@redhat.com>
2014-11-21 10:26:23 +0100

    Parse xccdf:rule-result[s] from incomming ARF to database.

--- app/models/scaptimony/arf_report.rb
+++ app/models/scaptimony/arf_report.rb
@@ -1,6 +1,8 @@
 require 'fileutils'
 require 'openscap'
 require 'openscap/ds/arf'
+require 'openscap/xccdf/testresult'
+require 'openscap/xccdf/ruleresult'
 require 'scaptimony/engine'
 
 module Scaptimony
@@ -17,6 +19,7 @@ module Scaptimony
       begin
         FileUtils.mkdir_p dir
         File.open(path, 'wb') { |f| f.write(data) }
+        save_dependent_entities
       rescue StandardError => e
         logger.error "Could not store ARF to '#{path}': #{e.message}"
         raise e
@@ -40,6 +43,25 @@ module Scaptimony
     end
 
     private
+    def save_dependent_entities
+      begin
+        OpenSCAP.oscap_init
+        arf = OpenSCAP::DS::Arf.new path
+        test_result = arf.test_result
+        test_result.rr.each {|rr_id, rr|
+          rule = ::Scaptimony::XccdfRule.where(:xid => rr_id).first_or_create!
+          self.xccdf_rule_results.create!(:xccdf_rule_id => rule.id, :xccdf_result_id => XccdfResult.f(rr.result).id)
+        }
+      rescue StandardError => e
+        self.xccdf_rule_results.destroy_all
+        raise e
+      ensure
+        test_result.destroy unless test_result.nil?
+        arf.destroy unless arf.nil?
+        OpenSCAP.oscap_cleanup
+      end
+    end
+
     def path
       "#{dir}/#{digest}.xml.bz2"
     end
