eccce3f37e425f1830716849df48716d67c377d9
Šimon Lukašík <slukasik@redhat.com>
2014-12-10 20:50:50 +0100

    Map ArfReport to Asset.

--- app/models/scaptimony/asset.rb
+++ app/models/scaptimony/asset.rb
@@ -1,5 +1,6 @@
 module Scaptimony
   class Asset < ActiveRecord::Base
     has_and_belongs_to_many :policies, :join_table => :scaptimony_assets_policies
+    has_many :arf_reports, :dependent => :destroy
   end
 end
