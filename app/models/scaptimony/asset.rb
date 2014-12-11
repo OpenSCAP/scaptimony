02caf8e0f25b54f1ab7ad6dc9207d2e1176d725f
Šimon Lukašík <slukasik@redhat.com>
2014-12-11 17:04:38 +0100

    policy_reports lists only Assets with the ArfReport

--- app/models/scaptimony/asset.rb
+++ app/models/scaptimony/asset.rb
@@ -2,5 +2,7 @@ module Scaptimony
   class Asset < ActiveRecord::Base
     has_and_belongs_to_many :policies, :join_table => :scaptimony_assets_policies
     has_many :arf_reports, :dependent => :destroy
+
+    scope :policy_reports, lambda { |*args| includes(:arf_reports).where(:scaptimony_arf_reports => {:policy_id => args.first.id}) }
   end
 end
