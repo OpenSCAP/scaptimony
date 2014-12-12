15bb40bafd35fc61206d2fc3f0a034401428c54f
Šimon Lukašík <slukasik@redhat.com>
2014-12-12 12:15:49 +0100

    Use named argument to increase readability.

--- app/models/scaptimony/asset.rb
+++ app/models/scaptimony/asset.rb
@@ -3,22 +3,22 @@ module Scaptimony
     has_and_belongs_to_many :policies, :join_table => :scaptimony_assets_policies
     has_many :arf_reports, :dependent => :destroy
 
-    scope :policy_reports, lambda { |*args| includes(:arf_reports).where(:scaptimony_arf_reports => {:policy_id => args.first.id}) }
-    scope :comply_with, lambda { |*args|
+    scope :policy_reports, lambda { |policy| includes(:arf_reports).where(:scaptimony_arf_reports => {:policy_id => policy.id}) }
+    scope :comply_with, lambda { |policy|
       joins("-- this is emo, we need some hipsters to rewrite this using arel
              INNER JOIN (select asset_id, max(id) AS id
              FROM scaptimony_arf_reports
-             WHERE policy_id = #{args.first.id}
+             WHERE policy_id = #{policy.id}
              GROUP BY asset_id) scaptimony_arf_reports
              ON scaptimony_arf_reports.asset_id = scaptimony_assets.id").
       joins('INNER JOIN scaptimony_arf_report_breakdowns
              ON scaptimony_arf_reports.id = scaptimony_arf_report_breakdowns.arf_report_id').
       where(:scaptimony_arf_report_breakdowns => {:failed => 0, :othered => 0})
     }
-    scope :incomply_with, lambda { |*args|
+    scope :incomply_with, lambda { |policy|
       joins("INNER JOIN (select asset_id, max(id) AS id
              FROM scaptimony_arf_reports
-             WHERE policy_id = #{args.first.id}
+             WHERE policy_id = #{policy.id}
              GROUP BY asset_id) scaptimony_arf_reports
              ON scaptimony_arf_reports.asset_id = scaptimony_assets.id").
       joins('INNER JOIN scaptimony_arf_report_breakdowns
