67a28ba16e7a23df928351dda9b4b3d9468c879c
Šimon Lukašík <slukasik@redhat.com>
2014-12-12 13:12:34 +0100

    policy_reports_missing lists Assets without reports for given policy

--- app/models/scaptimony/asset.rb
+++ app/models/scaptimony/asset.rb
@@ -4,6 +4,7 @@ module Scaptimony
     has_many :arf_reports, :dependent => :destroy
 
     scope :policy_reports, lambda { |policy| includes(:arf_reports).where(:scaptimony_arf_reports => {:policy_id => policy.id}) }
+    scope :policy_reports_missing, lambda { |policy| where("id NOT IN (select asset_id from scaptimony_arf_reports where policy_id = #{policy.id})") }
     scope :comply_with, lambda { |policy| last_arf(policy).breakdown.
       last_arf(policy).breakdown.where(:scaptimony_arf_report_breakdowns => {:failed => 0, :othered => 0})
     }
