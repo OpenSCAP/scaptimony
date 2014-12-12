2d828ffa5cc29a31a1847e1e939095dfff8966de
Šimon Lukašík <slukasik@redhat.com>
2014-12-12 12:22:26 +0100

    Refactor: Extract scope: last_arf

--- app/models/scaptimony/asset.rb
+++ app/models/scaptimony/asset.rb
@@ -4,26 +4,23 @@ module Scaptimony
     has_many :arf_reports, :dependent => :destroy
 
     scope :policy_reports, lambda { |policy| includes(:arf_reports).where(:scaptimony_arf_reports => {:policy_id => policy.id}) }
-    scope :comply_with, lambda { |policy|
+    scope :comply_with, lambda { |policy| last_arf(policy).
+      joins('INNER JOIN scaptimony_arf_report_breakdowns
+             ON scaptimony_arf_reports.id = scaptimony_arf_report_breakdowns.arf_report_id').
+      where(:scaptimony_arf_report_breakdowns => {:failed => 0, :othered => 0})
+    }
+    scope :incomply_with, lambda { |policy| last_arf(policy).
+      joins('INNER JOIN scaptimony_arf_report_breakdowns
+             ON scaptimony_arf_reports.id = scaptimony_arf_report_breakdowns.arf_report_id').
+      where('scaptimony_arf_report_breakdowns.failed != 0') #TODO:RAILS-4.0: rewrite with: where.not()
+    }
+    scope :last_arf, lambda { |policy|
       joins("-- this is emo, we need some hipsters to rewrite this using arel
              INNER JOIN (select asset_id, max(id) AS id
              FROM scaptimony_arf_reports
              WHERE policy_id = #{policy.id}
              GROUP BY asset_id) scaptimony_arf_reports
-             ON scaptimony_arf_reports.asset_id = scaptimony_assets.id").
-      joins('INNER JOIN scaptimony_arf_report_breakdowns
-             ON scaptimony_arf_reports.id = scaptimony_arf_report_breakdowns.arf_report_id').
-      where(:scaptimony_arf_report_breakdowns => {:failed => 0, :othered => 0})
-    }
-    scope :incomply_with, lambda { |policy|
-      joins("INNER JOIN (select asset_id, max(id) AS id
-             FROM scaptimony_arf_reports
-             WHERE policy_id = #{policy.id}
-             GROUP BY asset_id) scaptimony_arf_reports
-             ON scaptimony_arf_reports.asset_id = scaptimony_assets.id").
-      joins('INNER JOIN scaptimony_arf_report_breakdowns
-             ON scaptimony_arf_reports.id = scaptimony_arf_report_breakdowns.arf_report_id').
-      where('scaptimony_arf_report_breakdowns.failed != 0') #TODO:RAILS-4.0: rewrite with: where.not()
+             ON scaptimony_arf_reports.asset_id = scaptimony_assets.id")
     }
   end
 end
