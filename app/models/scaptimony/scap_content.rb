d17d06bdfd5ec363a4356cd3fac045dde78b1362
Shlomi Zadok <shlomi@ben-hanna.com>
2014-12-14 15:43:43 +0200

    store scap_content in DB

--- app/models/scaptimony/scap_content.rb
+++ app/models/scaptimony/scap_content.rb
@@ -1,5 +1,4 @@
-require 'digest'
-require 'fileutils'
+require 'digest/sha2'
 require 'openscap/ds/sds'
 require 'openscap/source'
 require 'openscap/xccdf/benchmark'
@@ -8,24 +7,19 @@ require 'scaptimony/engine'
 module Scaptimony
   class DataStreamValidator < ActiveModel::Validator
     def validate(scap_content)
-      if !scap_content.new_record?
-        return true if scap_content.scap_file.nil?
+      unless scap_content.new_record?
+        return true if scap_content.scap_file_changed?
         scap_content.errors[:base] << _("Cannot change uploaded file while editing content.")
         return false
       end
+
       if scap_content.scap_file.nil?
         scap_content.errors[:base] << _("Please select file for upload.")
         return false
       end
 
-      existing = ScapContent.where(:digest => scap_content.digest).first
-      if !existing.nil?
-        scap_content.errors[:base] << _("This file has been already uploaded as '#{existing.title}'.")
-        return false
-      end
-
       allowed_type = 'SCAP Source Datastream'
-      if scap_content.source.type != allowed_type
+      if scap_content.source.try(:type) != allowed_type
         scap_content.errors[:base] << _("Uploaded file is not #{allowed_type}.")
         return false
       end
@@ -45,26 +39,12 @@ module Scaptimony
     validates_with Scaptimony::DataStreamValidator
     validates :title, :presence => true
     validates :digest, :presence => true
-    attr_accessor :scap_file
+    validates :scap_file, :presence => true, :uniqueness => true
 
-    def store
-      if valid_store_attempt
-        begin
-          FileUtils.mkdir_p dir
-          source.save path
-          return false if !save
-          create_profiles
-        rescue StandardError => e
-          errors[:base] << e.message
-          return false
-        end
-      else
-        save
-      end
-    end
+    after_create :create_profiles
 
-    def valid_store_attempt
-      new_record? and !@scap_file.nil?
+    def to_label
+      title
     end
 
     def source
@@ -72,37 +52,25 @@ module Scaptimony
     end
 
     def digest
-      self[:digest] ||= Digest::SHA256.hexdigest "#{@scap_file}"
-    end
-
-    def path
-      "#{dir}/#{digest}"
+      self[:digest] ||= Digest::SHA256.hexdigest "#{scap_file}"
     end
 
     private
     def source_init
       OpenSCAP.oscap_init
-      if new_record?
-        OpenSCAP::Source.new(:content => @scap_file, :path => path)
-      else
-        OpenSCAP::Source.new path
-      end
-    end
-
-    def dir
-      "#{Scaptimony::Engine.dir}/content"
+      OpenSCAP::Source.new(:content => scap_file)
     end
 
     def create_profiles
-        sds = ::OpenSCAP::DS::Sds.new source
-        bench_source = sds.select_checklist!
-        bench = ::OpenSCAP::Xccdf::Benchmark.new bench_source
-        bench.profiles.each { |key, profile|
-          scap_content_profiles.create!(:profile_id => key, :title => profile.title)
-        }
-        bench.destroy
-        sds.destroy
-        true
-      end
+      sds          = ::OpenSCAP::DS::Sds.new source
+      bench_source = sds.select_checklist!
+      bench        = ::OpenSCAP::Xccdf::Benchmark.new bench_source
+      bench.profiles.each { |key, profile|
+        scap_content_profiles.create!(:profile_id => key, :title => profile.title)
+      }
+      bench.destroy
+      sds.destroy
+      true
+    end
   end
 end
