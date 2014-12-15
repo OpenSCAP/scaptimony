ab9a577e98f0eafa61943d20c9ead84aa788b96e
Shlomi Zadok <shlomi@ben-hanna.com>
2014-12-15 11:26:35 +0200

    fixes OpenSCAP/foreman_openscap#4 - Support deletion of scap_content

--- db/migrate/20141104164201_create_scaptimony_scap_contents.rb
+++ db/migrate/20141104164201_create_scaptimony_scap_contents.rb
@@ -1,10 +1,7 @@
 class CreateScaptimonyScapContents < ActiveRecord::Migration
   def change
     create_table :scaptimony_scap_contents do |t|
-      t.string :digest, limit: 128
-
       t.timestamps
     end
-    add_index :scaptimony_scap_contents, :digest, unique: true
   end
 end
