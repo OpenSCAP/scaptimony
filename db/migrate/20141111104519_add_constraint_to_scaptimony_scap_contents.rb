02e4f76bcd1a795c3104f3fc36a756f5a9dfada3
Marek Hulan <mhulan@redhat.com>
2015-01-07 09:22:06 +0100

    Fix failing migration

--- db/migrate/20141111104519_add_constraint_to_scaptimony_scap_contents.rb
+++ db/migrate/20141111104519_add_constraint_to_scaptimony_scap_contents.rb
@@ -1,6 +1,5 @@
 class AddConstraintToScaptimonyScapContents < ActiveRecord::Migration
   def change
     change_column :scaptimony_scap_contents, :title, :string, :null => false
-    change_column :scaptimony_scap_contents, :digest, :string, :null => false
   end
 end
